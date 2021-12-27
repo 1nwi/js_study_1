1333af5b45f26cff3604f0917bf99b20cac8f3ff
1nwi <lllhn@naver.com>
2021-12-27 15:07:58 +0900

    [21.12.27] # project v2

---
+++ tesst.js
@@ -0,0 +1,14 @@
+
+
+const a = 5;
+const b = 3;
+
+const star ='';
+
+for(let i = 1; i <= b; i++) {
+    for (let j = 1; j <= a; j++) {
+        star = '*';
+    }
+    star = '\n';
+}
+console.log(star);
